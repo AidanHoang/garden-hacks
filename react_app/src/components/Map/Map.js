6463b5b700937e26969c63e5fbad32d3952e8772
YeetYoshi <slapblackjack8@gmail.com>
2021-06-19 16:04:59 -0700

    Update Map.js

--- react_app/src/components/Map/Map.js
+++ react_app/src/components/Map/Map.js
@@ -131,9 +131,9 @@ function Map() {
         },
         {
             id: 15,
-            name: "Portland Japanese Garden, Orlando",
+            name: "Portland Japanese Garden, Oregon",
             details: "64 live cases",
-            position: { lat: 45.51953557387492, lng: -122.70700814798617 },
+            position: { lat: 45.5197235068326, lng: -122.70681502893103 },
             icon: iconList.safe
         },
         {
@@ -240,6 +240,90 @@ function Map() {
             details: "264 live cases",
             position: { lat: 36.41038534052995, lng: -114.90080332373498 },
             icon: iconList.moderate
+        },
+        {
+            id: 31,
+            name: "McCrory Gardens, South Dakota",
+            details: "62 live cases",
+            position: { lat: 44.47955818679794, lng: -96.82828370030334 },
+            icon: iconList.safe
+        },
+        {
+            id: 32,
+            name: "Wister Gardens, Mississipi",
+            details: "512 live cases",
+            position: { lat: 34.27447092610714, lng: -90.52806233315029 },
+            icon: iconList.danger
+        },
+        {
+            id: 33,
+            name: "The Oregon Garden, Oregon",
+            details: "352 live cases",
+            position: { lat: 44.99497885006193, lng: -122.79222594066185 },
+            icon: iconList.moderate
+        },
+        {
+            id: 34,
+            name: "Bellingrath Gardens, Alabama",
+            details: "264 live cases",
+            position: { lat: 30.748253227436766, lng: -88.12982960134953 },
+            icon: iconList.moderate
+        },
+        {
+            id: 35,
+            name: "State Botanical Garden, Kentucky",
+            details: "612 live cases",
+            position: { lat: 38.23033123706244, lng: -84.51917911991526 },
+            icon: iconList.danger
+        },
+        {
+            id: 36,
+            name: "Myriad Botanical Gardens, Oklahoma",
+            details: "27 live cases",
+            position: { lat: 35.46646706837911, lng: -97.51777855368911 },
+            icon: iconList.safe
+        },
+        {
+            id: 37,
+            name: "Fort Worth Botanic Garden, Texas",
+            details: "382 live cases",
+            position: { lat: 32.74898604422186, lng: -97.36458854333615 },
+            icon: iconList.moderate
+        },
+        {
+            id: 38,
+            name: "Conservation Garden Park, Utah",
+            details: "172 live cases",
+            position: { lat: 41.2744860750826, lng: -112.03017556856763 },
+            icon: iconList.safe
+        },
+        {
+            id: 39,
+            name: "Gardens at the Springs Preserve, Nevada",
+            details: "312 live cases",
+            position: { lat: 36.367400958631954, lng: -115.22525041522562 },
+            icon: iconList.moderate
+        },
+        {
+            id: 40,
+            name: "Winnemucca Community Garden, Nevada",
+            details: "64 live cases",
+            position: { lat: 41.18338158683258, lng: -117.76519974369465 },
+            icon: iconList.safe
+        },
+        {
+            id: 41,
+            name: "The Veterans Memorial Garden, Nebraska",
+            details: "128 live cases",
+            position: { lat: 41.376140359644175, lng: -96.80537556090255 },
+            icon: iconList.safe
+        },
+        {
+            id: 41,
+            name: "Gatiss Gardens, Montana",
+            details: "612 live cases",
+            position: { lat: 48.803602244723315, lng: -114.06697156604832 },
+            icon: iconList.danger
         },
        
     ];
